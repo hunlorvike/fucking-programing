9ea9d56c316d8c8fdcc8fb8a49164788cfecc639
Nguyễn Việt Hưng <hungcutedethuongg@gmail.com>
2024-10-25 16:09:11 +0700

    convert python -> typescript

---
+++ Interview Preparation/Coding Challenges/LeetCode/0000-0099/0003. Longest Substring Without Repeating Characters/Solution.ts
@@ -0,0 +1,15 @@
+function lengthOfLongestSubstring(s: string): number {
+  const subString = new Set<string>();
+  let answer = 0;
+  let start = 0;
+  for (let index = 0; index < s.length; index++) {
+    const char = s[index];
+    while (subString.has(char)) {
+      subString.delete(s[start]);
+      start++;
+    }
+    subString.add(char);
+    answer = Math.max(answer, index - start + 1);
+  }
+  return answer;
+}
